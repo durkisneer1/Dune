05a8ad3949ea4868994a1c0cb72bd6af3aa74818
durk <martinez.d048432@stu.sanjac.edu>
2024-03-29 01:45:58 -0500

    fixed pixel jitter with frect

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT), pg.SCALED)
+        pg.display.set_caption("Dune")
         self.clock = pg.Clock()
 
         self.keys = ()
@@ -18,9 +19,9 @@ class Game:
         tile_set = load_pygame("assets/terrain.tmx")
         self.collision_tiles = []
         self.all_tiles = []
-        load_tmx_layers(tile_set, "Sand", self.all_tiles)
-        load_tmx_layers(tile_set, "Wall", (self.collision_tiles, self.all_tiles))
-        load_tmx_layers(tile_set, "Tree", self.all_tiles)
+        load_tmx_layers(self, tile_set, "Sand", self.all_tiles)
+        load_tmx_layers(self, tile_set, "Wall", (self.collision_tiles, self.all_tiles))
+        load_tmx_layers(self, tile_set, "Tree", self.all_tiles)
 
         self.player = Player(self, [tile.rect for tile in self.collision_tiles])
 
@@ -35,7 +36,7 @@ class Game:
 
     def run(self):
         while self.running:
-            self.dt = self.clock.tick(60) / 1000
+            self.dt = self.clock.tick() / 1000
             self.keys = pg.key.get_pressed()
 
             for event in pg.event.get():
@@ -45,7 +46,7 @@ class Game:
 
             self.screen.fill((30, 30, 30))
             for tile in self.all_tiles:
-                tile.draw(self.screen, self.camera)
+                tile.draw()
             self.player.draw()
 
             pg.display.flip()
--- tile.py
+++ tile.py
@@ -1,12 +1,19 @@
+from typing import TYPE_CHECKING
+
 import pygame as pg
+
+if TYPE_CHECKING:
+    from main import Game
 
 
 class Tile:
-    def __init__(self, pos: pg.Vector2, image: pg.Surface, name: str):
+    def __init__(self, game: "Game", pos: pg.Vector2, image: pg.Surface, name: str):
+        self.game = game
+
         self.name = name
         self.image = image
-        self.rect = self.image.get_rect(topleft=pos)
+        self.rect = self.image.get_frect(topleft=pos)
         self.hovered = False
 
-    def draw(self, screen: pg.Surface, offset: pg.Vector2):
-        screen.blit(self.image, self.rect.move(-offset))
+    def draw(self):
+        self.game.screen.blit(self.image, self.rect.move(-self.game.camera))
